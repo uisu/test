c2b8e2d47c8aa3e791a5fedc6d6b1470d1805339
uisu <uisugit@sina.com>
2015-07-17 11:34:58 +0800

    support CLI

--- bin/generator.js
+++ bin/generator.js
@@ -3,13 +3,25 @@
 var program = require('commander')
 var path = require('path')
 var fs = require('fs-extra-promise')
+var pkg = require('../package.json')
 
+// 命令行参数
+program.version(pkg.version)
+			 .option('-t, --template <static | nodejs>', 'web site template, default to static', 'static')
+		   .parse(process.argv);
 
-//var destPath = program.args.shift() || '.'
-var tmplName = 'static'
+// 检查模板名称
+var tmplName = program.template;
+if('static' !== tmplName && 'nodejs' !== tmplName){
+	console.error('no such template: ' + tmplName);
+	process.exit(1);
+}
+
+// 文件路径
 var srcPath = path.join(__dirname, '..', 'templates', tmplName)
 var destPath = path.join('.',tmplName)
 
+// 复制模板文件夹到当前路径
 fs.copyAsync(srcPath, destPath)
 	.then(function(){
 		console.log('copy template success')
